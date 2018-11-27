5530609b51f204572d2f1a379984bb895112b44a
ARIP <bjjarip@gmail.com>
2018-11-27 23:56:28 +0300

    dont know

--- app/src/main/java/com/example/rvca/p1/MainActivity.java
+++ app/src/main/java/com/example/rvca/p1/MainActivity.java
@@ -33,10 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
         for (int i = 0; i < 50; i++) {
 
-            Monolit m = new Monolit(i, i);
-            m.
-
-            pkt_list.add(new Sotoviy((double) i,  (double) i));
+            pkt_list.add(new Sotoviy((double) i, (double) i));
 
         }
 
